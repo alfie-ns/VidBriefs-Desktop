130b67da332c2a9ffe4cf3174ed2da334424a256
Alfie Nurse <100518222+alfie-ns@users.noreply.github.com>
2024-07-15 05:53:04 +0100

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -3,3 +3,5 @@ touch .env
 echo "# https://platform.openai.com/api-keys" >> .env
 echo "" >> .env
 echo "OPENAI_API_KEY=" >> .env
+
+i need fi make a script to login on openai keys and add a number in an iterated manner
