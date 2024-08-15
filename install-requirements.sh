4e8d52b40b071d47c312e85a0d73a50f7ab52916
alfie-ns <alfienurse@gmail.com>
2024-08-15 19:48:24 +0100

    update

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [[ -z "$VIRTUAL_ENV" ]]; then
     echo "Not inside a virtual environment. Exiting with failure." && exit 1
+else
+    pip install -r requirements.txt # -r requirements text file
+    pip install --upgrade pip
+    printf '\n%s\n' "$(tput bold)Requirements installed successfully$(tput sgr0)"
 fi
-pip install -r requirements.txt
-# -r requirements text file
-pip install --upgrade pip
-printf '\n%s\n' "$(tput bold)Requirements installed successfully$(tput sgr0)"
+
+
 
