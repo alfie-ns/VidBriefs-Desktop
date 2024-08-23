8ee05f5423cfd3291fbba792ceb40eab3cd3775e
alfie-ns <alfienurse@gmail.com>
2024-08-23 01:52:46 +0100

    update

--- cp-desktop.sh
+++ cp-desktop.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Find all Python files in ai-scripts/ and copy them to the current directory
-#find ai-scripts/ -name "*.py" -type f -print0 | xargs -0 -I {} cp {} .
-#find ai-scripts/ -name "*.py" -type f -exec echo "File: {}" \; -exec cat {} \; -exec echo \;
 
 find ai-scripts/ -name "*.py" -type f -exec echo "File: {}" \; -exec cat {} \; -exec echo \; | pbcopy
 
--- git/pbd.sh
+++ git/pbd.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 if bash pu.sh; then
   cd ../..
