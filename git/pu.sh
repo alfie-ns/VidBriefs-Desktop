56cf1f422e93541220f6ae7bcb8c81a9df5b4f3c
alfie-ns <alfienurse@gmail.com>
2024-08-24 20:40:29 +0100

    Minor: Small changes, fixes, or updates

--- git/pu.sh
+++ git/pu.sh
@@ -2,10 +2,7 @@
 
 # Function to get commit importance
 get_commit_importance() {
-    echo "How important is this commit?"
-    echo "1. Minor: Small changes, fixes, or updates"
-    echo "2. Significant: New features, major improvements, or important fixes"
-    echo "3. Working: Newest fully-working version"
+    
     read -p "Enter the number (1-3): " importance
     # read input as importance variable used in case statement
 
@@ -20,7 +17,11 @@ get_commit_importance() {
 # Main script --------------------------------------------
 cd ..
 git add .
-
+echo ""
+echo "How important is this commit?"
+echo "1. Minor: Small changes, fixes, or updates"
+echo "2. Significant: New features, major improvements, or important fixes"
+echo "3. Working: Newest fully-working version"
 commit_message=$(get_commit_importance)
 git commit -m "$commit_message"
 
