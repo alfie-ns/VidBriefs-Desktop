c1b9db93545b6916735e0543bd871722da1bb109
alfie-ns <alfienurse@gmail.com>
2024-08-24 20:48:24 +0100

    Minor: Small changes, fixes, or updates

--- git/pu.sh
+++ git/pu.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Function to print bold text
+print_bold() {
+  BOLD=$(tput bold)
+  NORMAL=$(tput sgr0)
+  echo -e "${BOLD}$1${NORMAL}"
+}
+
 # Function to get commit importance
 get_commit_importance() {
-    
     read -p "Enter the number (1-3): " importance
     # read input as importance variable used in case statement
-
     case $importance in
         1) echo "Minor: Small changes, fixes, or updates";;
         2) echo "Significant: New features, major improvements, or important fixes";;
@@ -15,16 +20,17 @@ get_commit_importance() {
 }
 
 # Main script --------------------------------------------
-cd ..
+cd .. # backtrack to main directory
 git add .
-echo ""
-echo "How important is this commit?"
+
+echo -e "\nCommit importance:"
 echo "1. Minor: Small changes, fixes, or updates"
 echo "2. Significant: New features, major improvements, or important fixes"
-echo "3. Working: Newest fully-working version"
-commit_message=$(get_commit_importance)
+echo -e "3. Working: Newest fully-working version\n"
+
+commit_message=$(get_commit_importance) # call function
 git commit -m "$commit_message"
 
 git push origin main
 echo -e '\nLocal repo pushed to remote origin\n'
-echo "Commit message: $commit_message"
+print_bold "Commit message: $commit_message"
