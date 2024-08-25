b212d85001d980fd520d64e1871d7ba2852b743a
alfie-ns <alfienurse@gmail.com>
2024-08-25 19:43:25 +0100

    Invalid choice. Using 'Minor' as default. Minor: Small changes, fixes, or updates

--- pu.sh
+++ pu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Function to print bold text -----------------------------
+# Function to print bold text ------------------------------
 print_bold() {
   BOLD=$(tput bold)
   NORMAL=$(tput sgr0)
@@ -8,14 +8,14 @@ print_bold() {
 }
 
 # Function to get commit importance
-get_commit_importance() { # read user input into importance variable
-    read -p "Enter the number (1-5): " importance
+get_commit_importance() { # read user input into importance variable, -p frag to specify that string as a prompt
+    read -rsn1 "Enter the importance (1-5): " importance 
     case $importance in # importance cases
-        1) echo "Trivial: Typos, formatting, or very minor changes";;
-        2) echo "Minor: Small changes, fixes, or updates";;
-        3) echo "Moderate: Notable improvements or additions";;
-        4) echo "Significant: Major features or important fixes";;
-        5) echo "Critical: Crucial updates or milestone achievements";;
+        1) echo "Trivial";;
+        2) echo "Minor";;
+        3) echo "Moderate";;
+        4) echo "Significant";;
+        5) echo "Milestone";;
         *) echo "Invalid choice. Using 'Minor' as default."; echo "Minor: Small changes, fixes, or updates";;
     esac # end case
 }
@@ -24,13 +24,13 @@ get_commit_importance() { # read user input into importance variable
 git add .
 
 print_bold "\nCommit importance:"
-echo "1. Trivial: Typos, formatting, or very minor changes"
-echo "2. Minor: Small changes, fixes, or updates"
-echo "3. Moderate: Notable improvements or additions"
-echo "4. Significant: Major features or important fixes"
-echo -e "5. Milestone: Crucial updates or milestone achievements\n"
+echo "1. Trivial"
+echo "2. Minor"
+echo "3. Moderate"
+echo "4. Significant"
+echo -e "5. Milestone\n"
 
-commit_message=$(get_commit_importance) # call function
+commit_message=$(get_commit_importance) # run 'get_commit_importance' function; store output in 'commit_message'
 git commit -m "$commit_message"
 
 git push origin main
